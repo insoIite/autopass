0a92a235a58034303fed2c7f12d994a403882442
fdugast <fabien.dugast@stormshield.eu>
2019-02-27 23:52:57 +0100

    autopass automatically enter login/password for ssh and webites entries

--- autopass.py
+++ autopass.py
@@ -14,27 +14,92 @@ def list_passwords():
 
 
 def get_fields(entry):
-    """ Get the fields from a gopass entry """
+    """
+    Get the fields of a gopass entry and returns it as dict
+    :entry: A gopass entry
+    :ptype: str
+    :return: A dict containing the fields of the entry
+    :rtype: dict
+
+    Example
+    go pass show entry:
+    aDummyPasswordXNOCGHZOIDHAO
+    comment:
+    url: fdu.com
+    username: dugast.fabien@gmail.com
+
+    return:
+    {
+        'pass': 'aDummyPasswordXNOCGHZOIDHAO',
+        'user': 'dugast.fabien@gmail.com',
+        'url': 'fdu.com,
+        'comment': ''
+    }
+    """
     cmd = [GOPASS, 'show', entry]
     res = {}
     output = subprocess.check_output(cmd)
     for line in output.decode().split('\n'):
         if ':' not in line:
-            res['password'] = line
+            res['pass'] = line
             continue
         sp = line.split(':')
         res[sp[0]] = sp[1].lstrip()
     return res
 
 def get_entry():
+    """
+    Get an entry from password list stored in gopass using rofi
+    :return: An entry from the password list
+    :rtype: str
+    """
     passwords = list_passwords()
     cmd = ["rofi", "-sync", "-no-auto-select", "-i", "-dmenu", "-p", "pass: "]
     rofi = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     out, _ = rofi.communicate(input=passwords)
     return out.decode().rstrip()
 
-if __name__ == '__main__':
-    entry = get_entry()
+def get_targeted_window():
+    """
+    Return the currently focused window
+    """
+    return subprocess.check_output(['/usr/bin/xdotool', 'getwindowfocus']).decode().rstrip()
 
-    res = get_fields(entry)
-    print(res)
+
+def get_autotype_command(entry, fields):
+    """
+    """
+    autotype = ['user', '!Tab', 'pass']
+
+    if 'ssh' in entry:
+        autotype = ['pass', 'Return']
+    elif 'website' in entry and 'autotype' in fields:
+        if 'autotype' in fields:
+            autotype = [value for value in fields['autotype'].split(' ')]
+    else:
+        if 'autotype' in fields:
+            autotype = [fields[value] for value in fields]
+    return autotype
+
+def do_type():
+    """
+    """
+    entry = get_entry()
+    fields = get_fields(entry)
+    autotype = get_autotype_command(entry, fields)
+
+    cmds = []
+    for val in autotype:
+        if val.startswith('!'):
+            cmds.append(['xdotool', 'key', val.replace('!', '')])
+        else:
+            cmds.append(['xdotool', 'type', fields[val]])
+
+    if 'confirm' not in fields or fields['confirm']:
+        cmds.append(['xdotool', 'key', 'Return'])
+
+    for cmd in cmds:
+        subprocess.check_call(cmd)
+
+if __name__ == '__main__':
+    do_type()
