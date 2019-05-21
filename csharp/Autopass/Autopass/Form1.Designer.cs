fb69898f23612abcfcbec212312b28084f4b908a
Fabien Dugast <dugast.fabien@gmail.com>
2019-05-21 21:58:52 +0200

    Add Autopass for csharp/windows

---
+++ csharp/Autopass/Autopass/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Autopass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwordsList = new System.Windows.Forms.ListView();
+            this.passwordHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // passwordsList
+            // 
+            this.passwordsList.BackColor = System.Drawing.SystemColors.Info;
+            this.passwordsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.passwordHeader});
+            this.passwordsList.Font = new System.Drawing.Font("Rockwell", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordsList.FullRowSelect = true;
+            this.passwordsList.GridLines = true;
+            this.passwordsList.Location = new System.Drawing.Point(12, 12);
+            this.passwordsList.Name = "passwordsList";
+            this.passwordsList.Size = new System.Drawing.Size(776, 563);
+            this.passwordsList.TabIndex = 0;
+            this.passwordsList.UseCompatibleStateImageBehavior = false;
+            this.passwordsList.View = System.Windows.Forms.View.Details;
+            this.passwordsList.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.passwordsList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            // 
+            // passwordHeader
+            // 
+            this.passwordHeader.Text = "Passwords";
+            this.passwordHeader.Width = 816;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 587);
+            this.Controls.Add(this.passwordsList);
+            this.Name = "Form1";
+            this.Text = "Autopass";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView passwordsList;
+        private System.Windows.Forms.ColumnHeader passwordHeader;
+    }
+}
+
---
+++ csharp/Autopass/Autopass/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections;
+
+//TODO Use env var for gopass path if it is not in $PATH
+
+namespace Autopass
+{
+    public partial class Form1 : Form
+    {
+        // Import GetForegroundWindow
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        // Import SetForegroundWindow
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        // Store current focus before initializing the Form
+        IntPtr currentFocus = GetForegroundWindow();
+
+        public Form1()
+        {
+            InitializeComponent();
+            fillList();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+                 
+        }
+
+        private void listView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                String item = passwordsList.FocusedItem.Text;
+                Hashtable entryHash = getPasswordEntryHashTable(item);
+                //SendKeys.Send()
+                foreach (string key in entryHash.Keys)
+                {
+                    Console.WriteLine(String.Format("{0}: {1}", key, entryHash[key]));
+                }
+                this.Visible = false;
+                SetForegroundWindow(currentFocus);
+                typeEntry(entryHash);
+            }
+        }
+        
+        private void fillList()
+        // Call gopass exe and return password entries from standart output
+        {
+            var passwordsEntries = execProgramAndReturnOutput("gopass", "ls --flat");
+            foreach (var el in passwordsEntries.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                ListViewItem item = new ListViewItem
+                {
+                    Text = el
+                };
+                passwordsList.Items.Add(item);
+            }
+        }
+        
+        private String execProgramAndReturnOutput(String prg, String args)
+        {
+            var process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.FileName = prg;
+            process.StartInfo.Arguments = args.ToString();
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output;
+        }
+
+        private Hashtable getPasswordEntryHashTable(String entry)
+        {
+            var passwordEntry = execProgramAndReturnOutput("gopass", $"show {entry}");
+            Hashtable entryHash = new Hashtable();
+            foreach (var el in passwordEntry.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (!el.Contains(':'))
+                {
+                    entryHash.Add("pass", el);
+                } else
+                {
+                    var splitedEntry = el.Split(new String[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+                    entryHash.Add(splitedEntry[0].Trim(), splitedEntry[1].Trim());
+                }
+            }
+            return entryHash;
+        }
+
+        private void typeEntry(Hashtable passwordEntry)
+        {
+            String autoTypeOpts = (String)passwordEntry["autotype"];
+            var autoTypeOpt = autoTypeOpts.Split(new String[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var opt in autoTypeOpt)
+            {
+                Trace.Write(opt);
+                if (passwordEntry.ContainsKey(opt))
+                {
+                    String value = (String)passwordEntry[opt];
+                    SendKeys.SendWait(value);
+                } else if (opt.Contains("!"))
+                {
+                    var _opt = opt.Replace("!", "");
+                    SendKeys.SendWait(opt);
+                } else
+                {
+                    SendKeys.SendWait(opt);
+                }
+            }
+        }
+    }
+
+    
+}
